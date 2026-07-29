2c2c4cb0f4a67d5de423113e5cda75d3be8b85c2
tungning24 <tungning.p@gmail.com>
2026-07-29 11:45:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='screen-gallery-v2',FILES=['./','index.html','style.css','script.js','manifest.webmanifest','icon_192.png','icon_512.png'];
+const CACHE='screen-gallery-v2',FILES=['./','index.html','style.css','script.js','manifest.webmanifest','icon_192.png','icon_512.png','mp3/light-rain.mp3','mp3/ocean-waves.mp3','mp3/rainy-with-birds.mp3','mp3/Rainy-Mood.m4a'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
